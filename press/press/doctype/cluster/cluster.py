ca4c39841a24756136cf4433ef0a22be97d0eac2
Balamurali M <balamurali@frappe.io>
2024-09-27 12:07:19 +0530

    fix(Cluster): typo

--- press/press/doctype/cluster/cluster.py
+++ press/press/doctype/cluster/cluster.py
@@ -89,7 +89,7 @@ class Cluster(Document):
 		"Server": "f",  # App server is last as it needs both proxy and db server
 	}
 
-	private_server: ClassVar[dict] = {
+	private_servers: ClassVar[dict] = {
 		# TODO: Uncomment these when they are implemented
 		# "Monitor Server": "p",
 		# "Log Server": "e",
