c283156101c70a6c2e6f472ed9a04364b33c604e
esitehoster <esitehoster@gmail.com>
2025-02-11 18:52:26 +0500

    Update deploy_candidate.py

--- press/press/doctype/deploy_candidate/deploy_candidate.py
+++ press/press/doctype/deploy_candidate/deploy_candidate.py
@@ -857,11 +857,9 @@ class DeployCandidate(Document):
 			app.use_cached = bool(self.use_app_cache)
 
 	def _prepare_build_directory(self):
-		    # Ensure the directory is marked as safe for Git
-		subprocess.run(
-        		["git", "config", "--add", "safe.directory", "/home/frappe/context/apps/frappe"],
-        		check=True,
-    		)
+		# Ensure the directory is marked as safe for Git
+		subprocess.run(['git', 'config', '--add', 'safe.directory', '/home/frappe/context/apps/frappe'],check=True, capture_output=True, text=True)
+		
 		build_directory = frappe.get_value("Press Settings", None, "build_directory")
 		if not os.path.exists(build_directory):
 			os.mkdir(build_directory)
